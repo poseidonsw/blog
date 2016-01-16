cd3277b3e656ad35fd56efd4dc02dd3d5cfd2c45
Steve <steve@Steves-MacBook-Pro.local>
2016-01-15 19:58:22 -0800

    Add parse connect

--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,29 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("3DqICYxfo0xyFGkyGQCsZBAukqiCYkVqLc1ea7Qz", "Di3Q18z7jTolpgnarR9DFRzoXnYDAR5FIbWtV9Q5");
 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+      model: Blog
     });
 
+    var blogs = new Blogs();
+
+    blogs.fetch({
+      success: function(blogs){
+        var blogsView = new BlogsView({ collection: blogs });
+        blogsView.render();
+        $('.main-container').html(blogsView.el);
+      },
+      error: function(blogs, error){
+        console.log(error);
+      }
+    });
+
+    var BlogsView =  Parse.View.extend({
+      template: Handlebars.compile($('#blogs-tpl').html()),
+      render: function(){
+        var collection = { blog: this.collection.toJSON() };
+        this.$el.html(this.template(collection));
+      }
+    });
 });
