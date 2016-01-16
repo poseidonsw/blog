d0a23483e18eb21ee76eaf149ad365cb3eda9756
Steve <steve@Steves-MacBook-Pro.local>
2016-01-15 22:00:05 -0800

    Add backbone update

--- js/blog.js
+++ js/blog.js
@@ -1,33 +1,36 @@
 $(function() {
 
-    Parse.$ = jQuery;
+  Parse.$ = jQuery;
 
-    // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("3DqICYxfo0xyFGkyGQCsZBAukqiCYkVqLc1ea7Qz", "Di3Q18z7jTolpgnarR9DFRzoXnYDAR5FIbWtV9Q5");
+  // Replace this line with the one on your Quickstart Guide Page
+  Parse.initialize("3DqICYxfo0xyFGkyGQCsZBAukqiCYkVqLc1ea7Qz", "Di3Q18z7jTolpgnarR9DFRzoXnYDAR5FIbWtV9Q5");
 
-    var Blog = Parse.Object.extend("Blog");
-    var Blogs = Parse.Collection.extend({
-      model: Blog
-    });
+  var Blog = Parse.Object.extend("Blog");
 
-    var blogs = new Blogs();
-
-    blogs.fetch({
-      success: function(blogs){
-        var blogsView = new BlogsView({ collection: blogs });
-        blogsView.render();
-        $('.main-container').html(blogsView.el);
-      },
-      error: function(blogs, error){
-        console.log(error);
+  var query = new Parse.Query("Blog");
+  query.find({
+    success: function(results) {
+      var blogs = new Array();
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+        blogs.push(object.toJSON());
       }
-    });
+      // results is an array of Parse.Object.
+      var blogsView = new BlogsView({ collection: blogs });
+      blogsView.render();
+      $('.main-container').html(blogsView.el);
+    },
+    error: function(error) {
+      // error is an instance of Parse.Error.
+      console.log(error);
+    }
+  });
 
-    var BlogsView =  Parse.View.extend({
-      template: Handlebars.compile($('#blogs-tpl').html()),
-      render: function(){
-        var collection = { blog: this.collection.toJSON() };
+  var BlogsView =  Backbone.View.extend({
+    template: Handlebars.compile($('#blogs-tpl').html()),
+    render: function(){
+        var collection = { blog: this.collection };
         this.$el.html(this.template(collection));
-      }
-    });
+    }
+  });
 });
